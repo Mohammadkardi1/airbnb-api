f3d8f0e969d25eec7fd791cc10d72d220715bb9a
Mohammadkardi1 <mohammadkardi1@gmail.com>
2023-07-28 15:21:58 +0300

    uplaod updates

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -27,9 +27,30 @@ export const getTrips = async (req, res) => {
     // const token = req.headers.authorization.split(" ")[1]
     // let decodedData = jwt.verify(token, process.env.JWT_SECRET)
     try {
-        const response = await bookingModel.find({user: req.userId})
+        const bookings = await bookingModel.find({user: req.userId})
         .populate('place').sort({ checkIn: 1 })
-        res.status(200).json({data: response})
+
+
+        const now = Date.now();
+        const { futureBookings, pastBookings } = bookings.reduce(
+          (acc, booking) => {
+            if (String(booking.place.owner) === req.userId) {
+              if (booking.checkOut > now) {
+                acc.futureBookings.push(booking);
+              } else {
+                acc.pastBookings.push(booking);
+              }
+            }
+            return acc;
+          },
+          { futureBookings: [], pastBookings: [] }
+        )
+        res.status(200).json({data: {futureBookings, pastBookings} })
+
+
+
+
+        // res.status(200).json({data: bookings})
     } catch (error) {
         res.status(404).json({message: error.message})
     }
@@ -39,10 +60,26 @@ export const getTrips = async (req, res) => {
 export const getBookingsOnProperties = async (req, res) => {
     try {
         const bookings = await bookingModel.find().populate('place').sort({ checkIn: 1 })
-        const response = bookings.filter((booking) => {
-            return String(booking.place.owner) === req.userId 
-          })
-        res.status(200).json({data: response})
+        // let futureBookings = []
+        // let pastBookings = []
+        // const response = bookings.filter((booking) => {
+        //     return String(booking.place.owner) === req.userId 
+        //   })
+        const now = Date.now();
+        const { futureBookings, pastBookings } = bookings.reduce(
+          (acc, booking) => {
+            if (String(booking.place.owner) === req.userId) {
+              if (booking.checkOut > now) {
+                acc.futureBookings.push(booking);
+              } else {
+                acc.pastBookings.push(booking);
+              }
+            }
+            return acc;
+          },
+          { futureBookings: [], pastBookings: [] }
+        )
+        res.status(200).json({data: {futureBookings, pastBookings} })
     } catch (error) {
         res.status(404).json({message: error.message})
     }
